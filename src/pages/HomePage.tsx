4d379734602cdd06a542b68dea46646dd6c32d3d
leczyckibartek <leczyckibartek@gmail.com>
2026-05-08 16:18:19 +0200

    feat: add routing with react-router and create initial pages

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+export function HomePage() {
+
+  return (
+    <>
+      <div>
+        <h1>Home</h1>
+  
+      </div>
+    </>
+  )
+}
+
---
+++ src/pages/TreePage.tsx
@@ -0,0 +1,12 @@
+
+export function TreePage() {
+  return (
+    <section>
+      <div>
+        <h1>Tree</h1>
+      
+      </div>
+    </section>
+  )
+}
+
